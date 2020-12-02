93f8f1b7f04b46c1ff5dea19a2c1659de5869913
Konrad Bełzowski <konrad.belzowski@gmail.com>
2020-12-02 14:24:11 +0100

    Initial commit

---
+++ Chat/Source/Functionalities/Home/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  Chat
+//
+//  Created by Konrad Belzowski on 02/12/2020.
+//
+
+import Foundation
+import UIKit
+
+public class HomeViewController: UIViewController {
+    
+}
